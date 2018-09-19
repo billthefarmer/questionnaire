92a7400338cc1ee17ee47dc940ce840147f8a023
Bill Farmer <williamjfarmer@yahoo.co.uk>
2018-09-19 16:37:45 +0100

    Update questionnaire

--- wordpress/js/report.js
+++ wordpress/js/report.js
@@ -21,7 +21,7 @@ jQuery(document).ready(function($) {
     // Create cookie
     function createCookie(name, value, days)
     {
-        let expires;
+        let expires = "";
 
 	if (days)
         {
@@ -29,9 +29,6 @@ jQuery(document).ready(function($) {
 	    date.setTime(date.getTime() + (days*24*60*60*1000));
 	    expires = "; expires=" + date.toGMTString();
 	}
-
-	else
-            expires = "";
 
 	document.cookie = name + "=" + value + expires + "; path=/";
     }
