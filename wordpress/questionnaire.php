d53d75176a214caddcf1b5cce65ef9f519cfe064
Bill Farmer <williamjfarmer@yahoo.co.uk>
2018-09-30 13:53:52 +0100

    Update questionnaire

--- wordpress/questionnaire.php
+++ wordpress/questionnaire.php
@@ -289,8 +289,8 @@ function questionnaire_report_shortcode($atts)
 
             // $pdf->SetFont('', 'B');
             // $pdf->MultiCell(0, 0, $type, 0, 'L');
-            new_text_line($pdf);
-            $pdf->SetFont('', '');
+            // new_text_line($pdf);
+            // $pdf->SetFont('', '');
             $pdf->MultiCell(0, 0, $desc, 0, 'L');
             new_text_line($pdf);
             $pdf->MultiCell(0, 0, $text, 0, 'L');
