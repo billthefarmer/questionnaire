f4dc4ef19cbf034c549644c595bbcccbdf2a0dd1
Bill Farmer <williamjfarmer@yahoo.co.uk>
2018-10-09 12:36:12 +0100

    Update questionnaire

--- wordpress/questionnaire.php
+++ wordpress/questionnaire.php
@@ -22,7 +22,7 @@ ini_set('display_errors', 1);
 // $tcpdf_present = include_once 'tcpdf/tcpdf.php';
 
 // Include compose code
-$compose_present = include_once 'vendor/autoload.php';
+$vendor_present = include_once 'vendor/autoload.php';
 
 // Start session
 if (empty(session_id()))
@@ -201,7 +201,7 @@ function questionnaire_questions_shortcode($atts)
 // Add the content if the shortcode is found.
 function questionnaire_report_shortcode($atts)
 {
-    global $compose_present;
+    global $vendor_present;
 
     // Create report
     function create_report($filename, $forename, $lastname)
@@ -483,7 +483,7 @@ function questionnaire_report_shortcode($atts)
     ob_start();
 
     // Check TCPDF
-    if ($compose_present)
+    if ($vendor_present)
         $fileuri = create_report($filename, $forename, $lastname);
 
     else
@@ -491,38 +491,6 @@ function questionnaire_report_shortcode($atts)
 
     // Send email
     // send_email($usermail, $forename, $lastname, $username, $filename);
-
-    // Start session
-    // if (empty(session_id()))
-    //     session_start();
-
-    // Check for Infusionsoft ids
-    $clientId = post_custom('clientId');
-    $clientSecret = post_custom('clientSecret');
-
-    $infusionsoft = new \Infusionsoft\Infusionsoft(array(
-        'clientId'     => $clientId,
-        'clientSecret' => $clientSecret,
-        'redirectUri'  => 'report'));
-
-    // If the serialized token is available in the session storage, we
-    // tell the SDK to use that token for subsequent requests.
-    if (isset($_SESSION['token']))
-        $infusionsoft->setToken(unserialize($_SESSION['token']));
-
-    // If we are returning from Infusionsoft we need to exchange the
-    // code for an access token.
-    if (isset($_GET['code']) and !$infusionsoft->getToken())
-        $_SESSION['token'] = serialize($infusionsoft->requestAccessToken($_GET['code']));
-
-    if ($infusionsoft->getToken())
-        // Save the serialized token to the current session for
-        // subsequent requests
-        $_SESSION['token'] = serialize($infusionsoft->getToken());
-
-    else
-        // MAKE INFUSIONSOFT REQUEST
-        echo '<p><a href="' . $infusionsoft->getAuthorizationUrl() . '">Click here to authorize</a></p>';
 
     ?>
 <div class="report-content">
